b4f2999647b980e75679eada97f2107bfcbb3dea
Miguel Ramos <miguel921433@gmail.com>
2023-12-25 17:36:18 -0500

    migraciones creadas e instalado talwind

---
+++ database/migrations/2023_12_25_201959_create_order_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('addres',255);
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
